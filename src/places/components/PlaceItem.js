8350417928ebe237fdff8335eac83f2ca936fa10
oumaimasb <96146890+oumaimasb@users.noreply.github.com>
2022-02-23 16:31:20 +0100

    Map done

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+import Map from "../../shared/components/UIElements/Map";
 import Modal from "../../shared/components/UIElements/Modal";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
@@ -23,7 +24,7 @@ const PlaceItem = (props) => {
         footer={<Button onClick={closeMap}>Close</Button>}
       >
         <div className="map-container">
-          <h2>The Map !</h2>
+          <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
       <li className="place-item">
